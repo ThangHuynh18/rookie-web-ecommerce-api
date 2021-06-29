41645494bdd06d7a068f82297d86ac6fd4f12825
ThangHuynh18 <65102117+ThangHuynh18@users.noreply.github.com>
2021-06-29 15:12:05 +0700

    create project
    create project WebWatch api with spring boot

---
+++ WebWatch/src/main/java/com/rookie/webwatch/WebWatchApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.webwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWatchApplication.class, args);
+    }
+
+}
